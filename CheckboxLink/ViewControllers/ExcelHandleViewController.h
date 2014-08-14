c47b78184d87939453527d790f4434324dfdd14b
Yuichi Fujiki <yuichi@chaione.com>
2014-08-14 13:17:44 -0500

    Test both xls/xlsx files. Check that it can both read/write checkboxes.

---
+++ CheckboxLink/ViewControllers/ExcelHandleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExcelHandleViewController.h
+//  CheckboxLink
+//
+//  Created by Yuichi Fujiki on 8/14/14.
+//  Copyright (c) 2014 yfujiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExcelHandleViewController : UIViewController
+
+@end
---
+++ CheckboxLink/ViewControllers/ExcelHandleViewController.m
@@ -0,0 +1,148 @@
+//
+//  ExcelHandleViewController.m
+//  CheckboxLink
+//
+//  Created by Yuichi Fujiki on 8/14/14.
+//  Copyright (c) 2014 yfujiki. All rights reserved.
+//
+
+#import "ExcelHandleViewController.h"
+#import <LibXL/LibXL.h>
+
+@interface ExcelHandleViewController ()
+
+- (IBAction)editXLS:(id)sender;
+- (IBAction)editXLSX:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *outputLabel;
+
+@property (nonatomic) BookHandle book;
+@property (nonatomic) SheetHandle sheet;
+
+@end
+
+@implementation ExcelHandleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - XLS
+
+- (NSString *)filePathForType:(NSString *)type {
+    NSString *documentDir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+    NSString *outPath = [[documentDir stringByAppendingPathComponent:@"output"] stringByAppendingPathExtension:type];
+
+    if ([[NSFileManager defaultManager] fileExistsAtPath:outPath]) {
+        return outPath;
+    }
+
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"sample" ofType:type];
+    return filePath;
+}
+
+- (void)readXLS:(NSString *)type {
+    NSString *filePath = [self filePathForType:type];
+    if ([[type lowercaseString] isEqualToString:@"xlsx"]) {
+        self.book = xlCreateXMLBook();
+    } else {
+        self.book = xlCreateBook();
+    }
+    
+    Boolean result = xlBookLoad(self.book, [filePath cStringUsingEncoding:NSUTF8StringEncoding]);
+
+    if (!result) {
+        NSLog(@"Failed to read sample.xlsx");
+    }
+
+    self.sheet = xlBookGetSheet(self.book, 0);
+}
+
+- (void)editXLS {
+    [self toggleRow:32 column:1];
+    [self toggleRow:33 column:2];
+    [self toggleRow:34 column:1];
+    [self toggleRow:34 column:2];
+    [self toggleRow:34 column:3];
+    [self toggleRow:35 column:2];
+    [self toggleRow:35 column:3];
+    [self toggleRow:35 column:6];
+
+    xlSheetSetTopLeftView(self.sheet, 0, 0);
+
+    //    for (int i = [self checkMarkRowMin]; i <= [self checkMarkRowMax]; i++) {
+    //        xlSheetSetRowHidden(self.sheet, i, true);
+    //    }
+}
+
+- (void)toggleRow:(NSInteger)row column:(NSInteger)column {
+    Boolean val = xlSheetReadBool(self.sheet, row, column, 0);
+    xlSheetWriteBool(self.sheet, row, column, !val, 0);
+}
+
+- (NSInteger)checkMarkRowMin {
+    return 32;
+}
+
+- (NSInteger)checkMarkRowMax {
+    return 35;
+}
+
+- (void)writeXLS:(NSString *)type {
+    NSString *documentDir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+    NSString *outPath = [[documentDir stringByAppendingPathComponent:@"output"] stringByAppendingPathExtension:type];
+
+    NSLog(@"Writing out excel file to %@", outPath);
+    self.outputLabel.text = outPath;
+
+    xlBookSave(self.book, [outPath cStringUsingEncoding:NSUTF8StringEncoding]);
+
+    xlBookRelease(self.book);
+}
+
+- (char *)c_filePathFrom:(NSString *)filePath {
+
+    char *c_filePath = malloc(256);
+    [filePath getCString:c_filePath maxLength:256 encoding:NSUTF32BigEndianStringEncoding];
+    return c_filePath;
+}
+
+- (IBAction)editXLS:(id)sender {
+    [self readXLS:@"xls"];
+    [self editXLS];
+    [self writeXLS:@"xls"];
+}
+
+- (IBAction)editXLSX:(id)sender {
+    [self readXLS:@"xlsx"];
+    [self editXLS];
+    [self writeXLS:@"xlsx"];
+}
+@end

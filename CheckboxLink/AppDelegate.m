a09ef8e81422dbe14dc9d1f49d2108ce9c4468ef
Yuichi Fujiki <yuichi@chaione.com>
2014-08-14 12:44:54 -0500

    Added libxl. Implemented checkbox-cell mirroring.

--- CheckboxLink/AppDelegate.m
+++ CheckboxLink/AppDelegate.m
@@ -7,15 +7,28 @@
 //
 
 #import "AppDelegate.h"
+#import <LibXL/LibXL.h>
+
+@interface AppDelegate()
+
+@property (nonatomic) BookHandle book;
+@property (nonatomic) SheetHandle sheet;
+
+@end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+
+    [self readXLS];
+    [self editXLS];
+    [self writeXLS];
+
     return YES;
 }
 
@@ -44,6 +57,63 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - XLS
+
+- (void)readXLS {
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"sample" ofType:@"xlsx"];
+    self.book = xlCreateXMLBook();
+    Boolean result = xlBookLoad(self.book, [filePath cStringUsingEncoding:NSUTF8StringEncoding]);
+
+    if (!result) {
+        NSLog(@"Failed to read sample.xlsx");
+    }
+
+    self.sheet = xlBookGetSheet(self.book, 0);
+}
+
+- (void)editXLS {
+    xlSheetWriteBool(self.sheet, 32, 1, true, 0);
+    xlSheetWriteBool(self.sheet, 33, 2, true, 0);
+    xlSheetWriteBool(self.sheet, 34, 1, false, 0);
+    xlSheetWriteBool(self.sheet, 34, 2, true, 0);
+    xlSheetWriteBool(self.sheet, 34, 3, true, 0);
+    xlSheetWriteBool(self.sheet, 35, 2, false, 0);
+    xlSheetWriteBool(self.sheet, 35, 3, true, 0);
+    xlSheetWriteBool(self.sheet, 35, 6, false, 0);
+
+    xlSheetSetTopLeftView(self.sheet, 0, 0);
+
+    for (int i = [self checkMarkRowMin]; i <= [self checkMarkRowMax]; i++) {
+        xlSheetSetRowHidden(self.sheet, i, true);
+    }
+}
+
+- (NSInteger)checkMarkRowMin {
+    return 32;
+}
+
+- (NSInteger)checkMarkRowMax {
+    return 35;
+}
+
+- (void)writeXLS {
+    NSString *documentDir = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+    NSString *outPath = [documentDir stringByAppendingPathComponent:@"output.xlsx"];
+
+    NSLog(@"Writing out excel file to %@", outPath);
+    
+    xlBookSave(self.book, [outPath cStringUsingEncoding:NSUTF8StringEncoding]);
+
+    xlBookRelease(self.book);
+}
+
+- (char *)c_filePathFrom:(NSString *)filePath {
+
+    char *c_filePath = malloc(256);
+    [filePath getCString:c_filePath maxLength:256 encoding:NSUTF32BigEndianStringEncoding];
+    return c_filePath;
 }
 
 @end
